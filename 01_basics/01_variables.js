5175d31e4249ea7cd6578ea042e082cf092b10df
Sayan Ghosh <116192838+Myselfsayan@users.noreply.github.com>
2024-03-26 15:57:51 +0000

    Variable and container done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const pass="121212"
+let email="s.g@gmail.com"
+let location="Kolkata"
+//password="1111"//This is not allowed
+location = "Bengaluru"
+console.table([pass,email,location])
---
+++ 01_basics/02_container.js
@@ -0,0 +1,12 @@
+"use strict";//use all js codes as newer version
+
+//number
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
