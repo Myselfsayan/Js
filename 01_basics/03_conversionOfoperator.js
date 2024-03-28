77c3f13cf208947b4b75a4ade3f54b5d59079675
Sayan Ghosh <116192838+Myselfsayan@users.noreply.github.com>
2024-03-28 15:49:49 +0000

    Conversion Of Datatypes Done

---
+++ 01_basics/03_conversionOfoperator.js
@@ -0,0 +1,28 @@
+let score =33
+console.log(typeof score)
+
+let score1 ="33"
+console.log(typeof score1)
+
+//Whenever we typecast the values , we must write first letter in caps.
+
+let valueInnumber = Number(score1)
+console.log(typeof valueInnumber)
+console.log(valueInnumber)
+
+let valueInstring = String(score)
+console.log(typeof valueInnumber)
+console.log(score)
+//"33"=> 33
+//"33abc"=>NaN
+//true=>1 , false=>0
+let isLoggedIn =1;
+let booleanisLoggedIn = Boolean(isLoggedIn);
+console.log(booleanisLoggedIn)
+
+let isLoggedIn1 =0;
+let booleanisLoggedIn1 = Boolean(isLoggedIn1);
+console.log(booleanisLoggedIn1)
+
+//""=>false
+//"gfdg"=>true
