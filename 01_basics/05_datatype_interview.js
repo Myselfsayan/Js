38840e290657abc797d3580136a562aeebfaadc1
Sayan Ghosh <116192838+Myselfsayan@users.noreply.github.com>
2024-03-31 14:51:51 +0000

    Datatype Done

---
+++ 01_basics/05_datatype_interview.js
@@ -0,0 +1,23 @@
+//Primitive: 7 types ----> String,Number,Boolean,null,undefined,Symbol,BigInt
+
+const val1=12
+const val2=12.4
+const val3=null
+let userEmail
+
+const id1=Symbol("123")
+const id2=Symbol("123")
+console.log(id1===id2)
+
+//Non Premitive(Reference)
+//Array,Object,Functions
+
+const heros=["Alok" , "Hayato"];
+let myObj={
+    name:"Sayan",
+    age:20,
+}
+
+const myFunc= function(){
+    console.log("Hello World")
+}
