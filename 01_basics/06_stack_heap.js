02df515a7a0bbf2b0cf6162ae5be8306a5966284
Sayan Ghosh <116192838+Myselfsayan@users.noreply.github.com>
2024-03-31 15:43:38 +0000

    Stack , Heap and String Done

---
+++ 01_basics/06_stack_heap.js
@@ -0,0 +1,4 @@
+//Memory are two types in Js
+//Stack(Primitive) , Heap(Non Primitive)
+
+//Its like pass by values and Pass by reference
---
+++ 01_basics/07_string.js
@@ -0,0 +1,6 @@
+const name="Sayan"
+const name1="Ghosh"
+
+console.log(`My first name is ${name} and my surname is ${name1}`);
+
+//Use MDN Reference to lear more methods
