7058e795a0edc8f9a4bb0a924a4a88a01795cc3b
Sayan Ghosh <116192838+Myselfsayan@users.noreply.github.com>
2024-03-31 14:29:33 +0000

    Comparision of datatype done

---
+++ 01_basics/04_comp_of_datatype.js
@@ -0,0 +1,14 @@
+console.log(2>1)
+console.log(2<1)
+console.log(2>=1)
+console.log(2<=1)
+console.log(2==1)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+//Similarly for "UNDEFINED"
+
+console.log("2"===2) //here datatype also compared
+
+
